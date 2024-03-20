2ef603c2cad9d6e248d3cfed1fcc1df0f9584a5a
Nick Cushman <cushmann572@gmail.com>
2024-03-19 22:36:06 -0700

    init: set up initial file structure and boilerplate for HotNavProvider component.

---
+++ src/context/HotNavProvider.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+export const HotNavigationProvider: React.FC = ({ }): ReactElement => {
+  console.log('HELLO WORLD FROM HOT-NAV!!!');
+
+  return (
+    <div>hey hey hey</div>
+  )
+}
---
+++ src/index.tsx
@@ -0,0 +1,1 @@
+export { HotNavigationProvider } from './context/HotNavProvider';
