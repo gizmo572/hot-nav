0f008f1e93ff70941b2c17525d0577ddd56ae7e4
Nick Cushman <cushmann572@gmail.com>
2024-03-19 23:11:25 -0700

    feat: have HotLink component accept an 'href' prop that it sets as value of href attribute in returned Link, create an interface for HotLink Props.

--- src/components/HotLink.tsx
+++ src/components/HotLink.tsx
@@ -4,13 +4,18 @@ import React, { ReactElement } from 'react';
 import Link from 'next/link';
 import { useHotNavigation } from '../context/HotNavProvider';
 
+interface HotLinkProps {
+  href: string;
+  children: React.ReactNode;
+}
 
-const HotLink: React.FC<{children: React.ReactNode}> = ({ children }): ReactElement => {
+
+const HotLink: React.FC<HotLinkProps> = ({ href, children }): ReactElement => {
   const { testVar } = useHotNavigation();
   console.log('testVar', testVar)
 
   return (
-    <Link href={'/'}>
+    <Link href={href}>
       {children}
     </Link>
 
